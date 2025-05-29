8f86bb677284e1eefeb88a30edce31664911d170
kazenone <kazenone@gmail.com>
2025-05-29 14:55:40 +0100

    Kaze's standard bot template

---
+++ config.js
@@ -0,0 +1,17 @@
+const testServer = {
+  id: '589466032662642689',
+  tradeCategory: '1187182891327823872',
+  notifyChannel: '589466032662642691'
+}
+
+const submarineBuilders = {
+  id: '607692512773013536',
+  tradeCategory: '610604606031921172',
+  notifyChannel: '772072783475245076'
+}
+
+module.exports = {
+  '589466032662642689': testServer,
+  '607692512773013536': submarineBuilders,
+  all: [testServer, submarineBuilders]
+};
