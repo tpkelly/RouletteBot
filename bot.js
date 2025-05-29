52625c0ab84074865054297c2f090b00e7e763f0
kazenone <kazenone@gmail.com>
2025-05-29 15:39:47 +0100

    Hook up to mongoDB

--- bot.js
+++ bot.js
@@ -1,72 +1,35 @@
 const { Client, Collection, IntentsBitField, ApplicationCommandType } = require('discord.js');
+const { MongoClient } = require('mongodb');
 const fs = require('fs');
 
 const client = new Client({
-  intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
+  intents: [IntentsBitField.Flags.Guilds],
   partials: ['MESSAGE', 'CHANNEL']
 });
 
 const auth = require('./auth.json');
-const config = require('./config.js');
 
 client.once('ready', async () => {
   client.user.setActivity("If broke, return to Kazenone");
   
-  client.deletedMessages = [];
+  client.mongo = new MongoClient(auth.mongodb).db();
   
   // Register commands
   client.commands = new Collection();
   const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-  // Discord does not like us authenticating with bot tokens, so we need a bearer token instead
-  const params = new URLSearchParams();
-  params.append('client_id', auth.clientId);
-  params.append('client_secret', auth.clientSecret);
-  params.append('grant_type', 'client_credentials');
-  params.append('scope', 'identify');
-
-  var tokenResponse = await fetch('https://discord.com/api/v10/oauth2/token', { method: 'POST', body: params, headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
-  var token = await tokenResponse.json();
-  
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
     
-    if (command.guildCommand) {
-      // Register guild application slash commands
-      for (const guildConfig of config.all) {
-        var guild = client.guilds.resolve(guildConfig.id);
-
-        if (guild)
-        {
-          var newCommand = await guild.commands.create({
-            name: command.name,
-            description: command.description,
-            options: command.options,
-            type: command.type || ApplicationCommandType.ChatInput,
-            defaultPermission: false,
-          });
-          
-          await fetch(`https://discord.com/api/v10/applications/${client.application.id}/guilds/${guildConfig.id}/commands/${newCommand.id}/permissions`, {
-              method: 'POST',
-              headers: { Authentication: `Bearer ${token.access_token}` },
-              body: {
-                permissions: guildConfig.adminRoles.map(x => ({ id: x, type: /* Role */ 1, permission: true }))
-              }
-            });
-        }
-      }
-    }
-    else {
-      // Register application slash commands
-      client.application.commands.create({
-        name: command.name,
-        description: command.description,
-        options: command.options,
-        type: command.type || ApplicationCommandType.ChatInput,
-        defaultPermission: true,
-      });
-    }
+    // Register application slash commands
+    client.application.commands.create({
+      name: command.name,
+      description: command.description,
+      options: command.options,
+      type: command.type || ApplicationCommandType.ChatInput,
+      defaultPermission: true,
+    });
   }
   
   // Register event handlers
