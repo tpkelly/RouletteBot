7dc8d33aeb37ec65f7990f3660e39905c3f6ebc4
kazenone <kazenone@gmail.com>
2025-05-29 15:25:35 +0100

    Missing method

--- common.js
+++ common.js
@@ -1,5 +1,17 @@
 const { WebhookClient } = require('discord.js');
 
+function styledEmbed(title, description, colour) {
+  return {
+      title: title,
+      description: description,
+      color: colour || 0xde153a,
+      footer: {
+        //iconURL: 'https://cdn.discordapp.com/attachments/1154346363061030974/1156663427595522094/Funky_little_icon_you_got_there.png',
+        text: 'Roulette'
+      }
+    }
+}
+
 function sendHook(hookId, hookToken, messageData) {
   var hook = new WebhookClient({ id: hookId, token: hookToken });
   setTimeout(() => hook.destroy(), 10000);
