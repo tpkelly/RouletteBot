b67981d93d0ca57d90e70a5abe7343414708cd8b
kazenone <kazenone@gmail.com>
2025-06-16 19:52:52 +0100

    Add manual draw command

---
+++ commands/draw.js
@@ -0,0 +1,20 @@
+const { PermissionsBitField, ApplicationCommandOptionType } = require('discord.js');
+const tasks = require('../tasks/rouletteTasks.js');
+
+module.exports = {
+  name: 'draw',
+  description: 'Manually draw the roulette matches',
+  options: [{ type: ApplicationCommandOptionType.Boolean, name: "dry-run", description: "If true, will only do a dry-run of the draw, and will not remove roles from users. Defaults to false", required: false }],
+  executeInteraction: async(interaction) => {
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+      await interaction.editReply({ embeds: [common.styledEmbed('Configuration', 'Only users with Manage Server permissions can configure the roulette settings')]});
+      return;
+    }
+    
+    var dryRun = interaction.options.getBoolean('dry-run') ?? false;
+    
+    var config = await interaction.client.mongo.collection('config').findOne({ _id: interaction.guild.id }) || {};
+    
+    await tasks.draw(interaction.client, config, dryRun);
+  }
+};
